920112be742c13ab82ce04771662e75f70d5e8ef
Artem Chystiakov <artem.ch31@gmail.com>
2024-08-14 20:16:26 +0300

    change testnet smt

--- deploy/config/localhost.ts
+++ deploy/config/localhost.ts
@@ -1,4 +1,4 @@
 export const chainName = "Localhost";
-export const registrationSMT = "0xBF926a23B4A0bcA301F97Ccd27358b55Dc4C7D3C";
+export const registrationSMT = "0xc1534912902BBe8C54626e2D69288C76a843bc0E";
 export const tssSigner = "0x038D006846a3e203738cF80A02418e124203beb2";
 export const votingName = "InitialVoting";
