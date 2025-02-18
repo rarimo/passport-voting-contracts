61bccff2f61e33653d9ad9d17063ec83e50e652e
Kyryl R <kyryl.ryabov@gmail.com>
2025-02-18 07:46:41 +0200

    Fixed deployment scripts and tests

--- test/voting/Voting.test.ts
+++ test/voting/Voting.test.ts
@@ -39,7 +39,7 @@ describe("Voting", () => {
     let proxy = await Proxy.deploy(await proposalsState.getAddress(), "0x");
     proposalsState = proposalsState.attach(await proxy.getAddress()) as ProposalsState;
 
-    await proposalsState.__ProposalsState_init(await proposalSMT.getAddress());
+    await proposalsState.__ProposalsState_init(await proposalSMT.getAddress(), 0n);
   }
 
   async function deployVoting() {
