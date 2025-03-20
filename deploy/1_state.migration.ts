1d96f321937cbe9b2b1f4fdfe79ef231940a1e59
Kyryl R <kyryl.ryabov@gmail.com>
2025-03-20 13:49:08 +0200

    Refactored deployment scripts

--- deploy/1_state.migration.ts
+++ deploy/1_state.migration.ts
@@ -1,5 +1,4 @@
-import { Deployer, Reporter } from "@solarity/hardhat-migrate";
-import { deployPoseidons, deployProxy } from "./helpers/helper";
+import { Deployer } from "@solarity/hardhat-migrate";
 
 import { ProposalSMT__factory, ProposalsState__factory } from "@ethers-v6";
 
@@ -8,11 +7,9 @@ import { getConfig } from "./config/config";
 export = async (deployer: Deployer) => {
   const config = (await getConfig())!;
 
-  await deployPoseidons(deployer, [2, 3]);
-
   let proposalSMT = await deployer.deploy(ProposalSMT__factory, { name: "ProposalSMT" });
 
-  const proposalsState = await deployProxy(deployer, ProposalsState__factory, "ProposalsState");
+  const proposalsState = await deployer.deployERC1967Proxy(ProposalsState__factory);
 
   await proposalsState.__ProposalsState_init(await proposalSMT.getAddress(), config.minFundingAmount);
 };
--- deploy/helpers/helper.ts
+++ deploy/helpers/helper.ts
@@ -1,9 +1,4 @@
 import { Deployer } from "@solarity/hardhat-migrate";
-import { EthersContract } from "@solarity/hardhat-migrate/dist/src/types/adapter";
-
-import { BaseContract } from "ethers";
-
-import { ERC1967Proxy__factory } from "@/generated-types/ethers";
 
 const { poseidonContract } = require("circomlibjs");
 
@@ -28,18 +23,4 @@ export async function deployPoseidons(deployer: Deployer, poseidonSizeParams: nu
   for (const size of poseidonSizeParams) {
     await deployPoseidon(size);
   }
-}
-
-export async function deployProxy<T, I = BaseContract>(
-  deployer: Deployer,
-  factory: EthersContract<T, I>,
-  name: string,
-) {
-  const implementation = (await deployer.deploy(factory, { name: name })) as BaseContract;
-
-  await deployer.deploy(ERC1967Proxy__factory, [await implementation.getAddress(), "0x"], {
-    name: `${name} Proxy`,
-  });
-
-  return await deployer.deployed(factory, `${name} Proxy`);
 }
