e4e3aa469d2c989f382d86fc0d029f60859cee60
Kyryl R <kyryl.ryabov@gmail.com>
2025-10-31 15:56:06 +0200

    Added IDCard voting

---
+++ contracts/mock/Helper.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import {TD1QueryProofNoirVerifier} from "@rarimo/passport-contracts/sdk/verifier/TD1QueryProofNoirVerifier.sol";
+import {TD3QueryProofNoirVerifier} from "@rarimo/passport-contracts/sdk/verifier/TD3QueryProofNoirVerifier.sol";
--- contracts/voting/IDCardVoting.sol
+++ contracts/voting/IDCardVoting.sol
@@ -9,12 +9,12 @@ import {BaseVoting} from "./BaseVoting.sol";
 
 import {ProposalsState} from "../state/ProposalsState.sol";
 
-contract PassportVoting is BaseVoting {
+contract IDCardVoting is BaseVoting {
     using PublicSignalsBuilder for uint256;
 
     uint256 public constant IDENTITY_LIMIT = type(uint32).max;
 
-    function __BioPassportVoting_init(
+    function __IDCardVoting_init(
         address registrationSMT_,
         address proposalsState_,
         address votingVerifier_
@@ -104,54 +104,7 @@ contract PassportVoting is BaseVoting {
         bytes32,
         uint256 currentDate_,
         bytes memory userPayload_
-    ) internal view override returns (uint256) {
-        (uint256 proposalId_, uint256[] memory vote_, UserData memory userData_) = abi.decode(
-            userPayload_,
-            (uint256, uint256[], UserData)
-        );
-
-        uint256 proposalEventId = ProposalsState(proposalsState).getProposalEventId(proposalId_);
-        ProposalRules memory proposalRules_ = getProposalRules(proposalId_);
-
-        /**
-         * By default we check that the identity is created before the identityCreationTimestampUpperBound (proposal start)
-         *
-         * ROOT_VALIDITY is subtracted to address the issue with multiaccounts if they are created right before the voting.
-         * The registration root will still be valid and a user may bring 100 roots to vote 100 times.
-         */
-        uint256 identityCreationTimestampUpperBound = proposalRules_
-            .identityCreationTimestampUpperBound -
-            IPoseidonSMT(getRegistrationSMT()).ROOT_VALIDITY();
-        uint256 identityCounterUpperBound = IDENTITY_LIMIT;
-
-        // If identity is issued after the proposal start, it should not be reissued more than identityCounterUpperBound
-        if (userData_.identityCreationTimestamp > 0) {
-            identityCreationTimestampUpperBound = userData_.identityCreationTimestamp;
-            identityCounterUpperBound = proposalRules_.identityCounterUpperBound;
-        }
-
-        uint256 builder_ = PublicSignalsBuilder.newPublicSignalsBuilder(
-            proposalRules_.selector,
-            userData_.nullifier
-        );
-        builder_.withCurrentDate(currentDate_, 1 days);
-        builder_.withEventIdAndData(
-            proposalEventId,
-            uint256(uint248(uint256(keccak256(abi.encode(vote_)))))
-        );
-        builder_.withSex(proposalRules_.sex);
-        builder_.withCitizenship(userData_.citizenship);
-        builder_.withTimestampLowerboundAndUpperbound(0, identityCreationTimestampUpperBound);
-        builder_.withIdentityCounterLowerbound(0, identityCounterUpperBound);
-        builder_.withBirthDateLowerboundAndUpperbound(
-            proposalRules_.birthDateLowerbound,
-            proposalRules_.birthDateUpperbound
-        );
-        builder_.withExpirationDateLowerboundAndUpperbound(
-            proposalRules_.expirationDateLowerBound,
-            PublicSignalsBuilder.ZERO_DATE
-        );
-
-        return builder_;
+    ) internal pure override returns (uint256) {
+        revert("TD3 voting is not supported.");
     }
 }
