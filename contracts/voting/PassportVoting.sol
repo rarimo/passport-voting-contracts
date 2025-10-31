9f831c70a6332c9f9e5da24043da279e847ff312
Kyryl R <kyryl.ryabov@gmail.com>
2025-10-31 15:13:38 +0200

    Added _buildPublicSignalsTD1 to the PassportVoting

--- contracts/voting/PassportVoting.sol
+++ contracts/voting/PassportVoting.sol
@@ -3,12 +3,13 @@ pragma solidity 0.8.28;
 
 import {IPoseidonSMT} from "@rarimo/passport-contracts/interfaces/state/IPoseidonSMT.sol";
 import {PublicSignalsBuilder} from "@rarimo/passport-contracts/sdk/lib/PublicSignalsBuilder.sol";
+import {PublicSignalsTD1Builder} from "@rarimo/passport-contracts/sdk/lib/PublicSignalsTD1Builder.sol";
 
 import {BaseVoting} from "./BaseVoting.sol";
 
 import {ProposalsState} from "../state/ProposalsState.sol";
 
-contract BioPassportVoting is BaseVoting {
+contract PassportVoting is BaseVoting {
     using PublicSignalsBuilder for uint256;
 
     uint256 public constant IDENTITY_LIMIT = type(uint32).max;
@@ -42,6 +43,61 @@ contract BioPassportVoting is BaseVoting {
         );
 
         ProposalsState(proposalsState).vote(proposalId_, userData_.nullifier, vote_);
+    }
+
+    function _buildPublicSignalsTD1(
+        bytes32,
+        uint256 currentDate_,
+        bytes memory userPayload_
+    ) internal view override returns (uint256) {
+        (uint256 proposalId_, uint256[] memory vote_, UserData memory userData_) = abi.decode(
+            userPayload_,
+            (uint256, uint256[], UserData)
+        );
+
+        uint256 proposalEventId = ProposalsState(proposalsState).getProposalEventId(proposalId_);
+        ProposalRules memory proposalRules_ = getProposalRules(proposalId_);
+
+        /**
+         * By default we check that the identity is created before the identityCreationTimestampUpperBound (proposal start)
+         *
+         * ROOT_VALIDITY is subtracted to address the issue with multiaccounts if they are created right before the voting.
+         * The registration root will still be valid and a user may bring 100 roots to vote 100 times.
+         */
+        uint256 identityCreationTimestampUpperBound = proposalRules_
+            .identityCreationTimestampUpperBound -
+            IPoseidonSMT(getRegistrationSMT()).ROOT_VALIDITY();
+        uint256 identityCounterUpperBound = IDENTITY_LIMIT;
+
+        // If identity is issued after the proposal start, it should not be reissued more than identityCounterUpperBound
+        if (userData_.identityCreationTimestamp > 0) {
+            identityCreationTimestampUpperBound = userData_.identityCreationTimestamp;
+            identityCounterUpperBound = proposalRules_.identityCounterUpperBound;
+        }
+
+        uint256 builder_ = PublicSignalsTD1Builder.newPublicSignalsBuilder(
+            proposalRules_.selector,
+            userData_.nullifier
+        );
+        builder_.withCurrentDate(currentDate_, 1 days);
+        builder_.withEventIdAndData(
+            proposalEventId,
+            uint256(uint248(uint256(keccak256(abi.encode(vote_)))))
+        );
+        builder_.withSex(proposalRules_.sex);
+        builder_.withCitizenship(userData_.citizenship);
+        builder_.withTimestampLowerboundAndUpperbound(0, identityCreationTimestampUpperBound);
+        builder_.withIdentityCounterLowerbound(0, identityCounterUpperBound);
+        builder_.withBirthDateLowerboundAndUpperbound(
+            proposalRules_.birthDateLowerbound,
+            proposalRules_.birthDateUpperbound
+        );
+        builder_.withExpirationDateLowerboundAndUpperbound(
+            proposalRules_.expirationDateLowerBound,
+            PublicSignalsBuilder.ZERO_DATE
+        );
+
+        return builder_;
     }
 
     function _buildPublicSignals(
